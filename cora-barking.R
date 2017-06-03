edebfae47cd922e359562f29a6318c8509ca3e60
Jeffrey K Naecker <jnaecker@gmail.com>
2017-06-03 18:47:28 -0400

    Plotted barks per hour facetted by days

--- cora-barking.R
+++ cora-barking.R
@@ -13,21 +13,20 @@ cora <- gs_ls() %>%
   .[["sheet_title"]] %>%
   map(gs_title) %>%
   map(~gs_read(., col_names = c("Time", "Device", "Level", "Test"))) %>%
-  reduce(rbind)
+  reduce(rbind) %>%
+  mutate(
+    Date = as.Date(Time, format = "%B %d, %Y at %I:%M%p"),
+    DateTime = as.POSIXct(strptime(Time, format = "%B %d, %Y at %I:%M%p"))
+  )
 
-cora$Date <- as.Date(cora$Time, format = "%B %d, %Y at %I:%M%p")
-cora$DateTime <- as.POSIXct(strptime(cora$Time, format = "%B %d, %Y at %I:%M%p"))
-
-minutes <- cora %>%
-  filter(DateTime >= "2017-04-24 08:00:00") %>%
-  select(DateTime) %>%
-  group_by(DateTime) %>%
-  summarize(count = n())
-
-ggplot(minutes, aes(x=DateTime, y=count)) + 
-  geom_lollipop() + 
-  scale_y_continuous(breaks = seq(0,20,2)) +
-  scale_x_datetime(date_breaks = "1 hour", date_labels="%I") +
-  theme_ipsum() +
-  labs(x = "Time", y = "Number of Barks")
-
+cora %>%
+  mutate(hour = hour(DateTime), day = date(DateTime)) %>%
+  group_by(hour, day) %>%
+  summarize(count = n()) %>%
+  ggplot(aes(x = hour, y = count)) + 
+    geom_lollipop() + 
+    scale_y_log10() +
+    scale_x_continuous(breaks = seq(0, 24, 1)) +
+    theme_ipsum() +
+    facet_wrap(~ day, ncol = 3) + 
+    labs(x = "Hour", y = "Number of Barks")
